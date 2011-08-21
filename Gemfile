b88e7204cfbcd3099896dd8cd216db62f026c1c4
Chris Small <metasoarous@gmail.com>
2011-08-21 16:00:46 -0700

    Just flushing out some of the basic application directory structure

---
+++ Gemfile
@@ -0,0 +1,9 @@
+
+source "http://rubygems.org"
+
+gem "nokogiri", "~> 1.5.0"
+gem "activerecord", "~> 3.0.10"
+
+gem "rspec", :require => "spec"
+
+
---
+++ application.rb
@@ -0,0 +1,22 @@
+require "rubygems"
+
+require "bundler/setup"
+
+Bundler.require :default
+
+ActiveRecord::Base.establish_connection(
+	adapter: 		"sqlite3",
+	database: 	"database.sqlite3",
+	pool: 			5,
+  timeout: 		5000
+)
+
+
+require "article"
+require "controller"
+
+
+controller = Controller.new
+
+controller.populate_db
+controller.parse_all
