776799d9755fba9971c1fa84f711060a504d201b
Chris Small <metasoarous@gmail.com>
2011-08-21 17:32:52 -0700

    A bit of base setup of the bot class

--- bot.rb
+++ bot.rb
@@ -4,4 +4,24 @@ class Bot
 	def get(address)
 	
 	end
+	
+	class DatabasePopulator < self
+		BASE_ADDRESS = "end.wikipedia.org/wiki/Special:AllPages/"
+	
+		def parse_indices
+			(0..9).each {|i| parse_index(i) }
+			("A".."Z").each do |l1|
+				("a".."z").each do |l2|
+					parse_index(l1 + l2)
+					parse_index(l1 + (l2.capitalize))
+				end
+			end
+		end
+		
+		def parse_index(index)
+			string = get(BASE_ADDRESS + index )
+			# scroll through pages of the index and parse (grab all the article 
+			# names using nokogiri madness) and create article records
+		end
+	end
 end
