b07a33e9783790f4143bb2c31f5ee036400f0a17
Chris Small <metasoarous@gmail.com>
2011-08-21 18:08:06 -0700

    Rewrote the bot file to work by iterating through the pages using next page and starting at the very first page

--- bot.rb
+++ bot.rb
@@ -2,29 +2,24 @@ require "net/http"
 
 class Bot
 	def get(address)
-	
+		Net::HTTP.get(address)
 	end
 	
 	class DatabasePopulator < self
 		BASE_ADDRESS = "end.wikipedia.org/wiki/Special:AllPages/"
 	
-		def parse_indices
-			(0..9).each {|i| parse_index(i) }
-			("A".."Z").each do |l1|
-				parse_index(l1)
-				("a".."z").each do |l2|
-					parse_index(l1 + l2)
-					parse_index(l1 + (l2.capitalize))
-				end
-			end
+		def populate_database
+			parse_index(BASE_ADDRESS + "!")
 		end
 		
-		def parse_index(index)
-			string = get(BASE_ADDRESS + index )
-			articles = some_fucked_up_parse_function_written_with_nokogiri
-			articles.each {|name| Article.create(:title => name)}
-			# scroll through pages of the index and parse (grab all the article 
-			# names using nokogiri madness) and create article records
+		def parse_index(address)
+			result = get(address)
+			@next_page = Parser.find_next_page(result)
+			@articles = Parser.find_all_titles(result)
+			
+			@articles.each {|name| Article.create(:title => name)}
+			
+			parse_index(@next_page) if @next_page
 		end
 	end
 end
